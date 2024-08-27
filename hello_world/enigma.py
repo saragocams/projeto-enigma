4b9b7d6c4a70fe273d00924394455676a015288b
prady001 <gabrielpac@al.insper.edu.br>
2024-08-27 12:17:08 -0300

    gerar_matrizes_de_permutacao

--- hello_world/enigma.py
+++ hello_world/enigma.py
@@ -5,11 +5,17 @@ def gerar_matrizes_de_permutacao(N : int) -> Tuple[np.ndarray, np.ndarray]:
     """
     Gera duas matrizes de permutação de tamanho N x N.
     """
-    raise NotImplementedError
+
+    I = np.eye(N)
+    P = np.random.permutation(I)
+    Q = np.random.permutation(I)
+
+    return P, Q
 
 def encriptar_enigma(mensagem : str,
               P : np.ndarray,
               Q : np.ndarray) -> str:
+    
     raise NotImplementedError
 
 def decriptar_enigma(mensagem_encriptada : str,
